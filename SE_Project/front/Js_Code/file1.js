680ad00bf4268acd759630fe3e4cde7b774d3fa4
Hassan <105183712+HasanMoham@users.noreply.github.com>
2024-05-04 17:35:03 +0300

    Add files via upload

---
+++ SE_Project/front/Js_Code/file1.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector(".navb"),
+  searchIcon = document.querySelector("#searchIcon");
+//   navOpenBtn = document.querySelector(".navOpenBtn"),
+//   navCloseBtn = document.querySelector(".navCloseBtn");
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all the card elements
+  var cards = document.querySelectorAll(".card.split");
+  
+  // Add click event listeners to each card
+  cards.forEach(function(card) {
+      card.addEventListener("click", function() {
+          // Get the URL from the data-url attribute of the clicked card
+          var url = card.getAttribute("data-url");
+          
+          // Navigate to the specified URL
+          window.location.href = url;
+      });
+  });
+});
+
+
